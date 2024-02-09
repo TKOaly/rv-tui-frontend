96a3b1bbc333dc216af756b741dc2b0f792410eb
Joona Kauppila <56773501+Jokauppi@users.noreply.github.com>
2024-02-09 23:30:22 +0200

    Add NoOverflowBox
    This container component should handle subcomponent overflow better by setting the child boxes size manually

---
+++ src/ui/components/NoOverflowBox.tsx
@@ -0,0 +1,33 @@
+import { Box } from "ink";
+import { useMeasurements } from "../../lib/dimensions.js";
+import { useStyles } from "../../state/style.js";
+
+type OwnProps = {
+	border?: boolean;
+	visible?: boolean;
+} & React.ComponentProps<typeof Box>;
+
+const NoOverflowBox = ({
+	border = false,
+	visible,
+	children,
+	...rest
+}: OwnProps) => {
+	const { ref, width, height } = useMeasurements();
+	const { borderStyle, borderColor } = useStyles();
+
+	<Box overflow="hidden" display={visible ? "flex" : "none"}>
+		<Box
+			ref={ref}
+			width={width}
+			height={height}
+			borderStyle={border ? borderStyle : undefined}
+			borderColor={borderColor}
+			overflow="hidden"
+			{...rest}
+		></Box>
+		{children}
+	</Box>;
+};
+
+export default NoOverflowBox;
